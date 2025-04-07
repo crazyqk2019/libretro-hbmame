a6fdde357dcdf086379090a70c04f7a1a0ca7062
crazyqk2019 <crazyq@gmail.com>
2025-04-07 11:34:10 +0800

    修正Python3下编译错误； 修正lib/netlist/plib/pstring.h高版本gcc编译错误； 修正3rdparty/sol2高版本gcc编译错误。

--- src/lib/netlist/plib/pstring.h
+++ src/lib/netlist/plib/pstring.h
@@ -17,6 +17,7 @@
 #include <string>
 #include <type_traits>
 #include <ostream>
+#include <cstdint>
 
 // ----------------------------------------------------------------------------------------
 // pstring: semi-immutable strings ...
